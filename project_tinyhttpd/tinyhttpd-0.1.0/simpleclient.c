37e2d1e27e9eb8c9f00ae7cf18617d42e1b6bec9
louis <louis.tin@outlook.com>
2016-07-13 09:39:11 +0800

    修改运行tinnyhttpd客户端程序

--- project_tinyhttpd/tinyhttpd-0.1.0/simpleclient.c
+++ project_tinyhttpd/tinyhttpd-0.1.0/simpleclient.c
@@ -12,12 +12,15 @@ int main(int argc, char *argv[])
 	struct sockaddr_in address;
 	int result;
 	char ch = 'A';
-
+	char buf[] = "GET / HTTP/1.1\n\n\n";
+	char get[1024] = {0};
+	// 创建sockfd
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	address.sin_port = htons(9734);
+	address.sin_port = htons(50329);
 	len = sizeof(address);
+	// 建立连接
 	result = connect(sockfd, (struct sockaddr *)&address, len);
 
 	if (result == -1)
@@ -25,9 +28,14 @@ int main(int argc, char *argv[])
 		perror("oops: client1");
 		exit(1);
 	}
-	write(sockfd, &ch, 1);
-	read(sockfd, &ch, 1);
-	printf("char from server = %c\n", ch);
+	write(sockfd,buf, sizeof(buf));/*发送请求*/
+	 read(sockfd,get, sizeof(get));/*接收返回数据*/
+	// 向socket写数据
+//	write(sockfd, &ch, 1);
+	// 从socket读数据
+//	read(sockfd, &ch, 1);
+//	printf("char from server = %c\n", ch);
+	printf("%s\n", get);
 	close(sockfd);
 	exit(0);
 }
